d48cb933c2c937d55c875a48451b15f5f317e511
Nuclear45 <welcometojob@yandex.ru>
2016-11-21 19:02:54 +0500

    Add files via upload
    Initial interfaces and classes that represent objects

---
+++ interfaces/ShowSquad.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+/**
+ * Created by Nuclear on 21.11.2016.
+ */
+public interface ShowSquad {
+}
---
+++ objects/Schedule.java
@@ -0,0 +1,7 @@
+package objects;
+
+/**
+ * Created by Nuclear on 21.11.2016.
+ */
+public class Schedule {
+}
